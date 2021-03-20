60f80465d1583a6deb3278bb589af8b2d4eaf001
6londe <6londe@gmail.com>
2021-03-20 19:14:06 +0900

    Fix naming

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import logs from './utils/logger';
+import log from './utils/logger';
 import config from '../config';
-import routes from './routes';
+import route from './routers';
 
 const { server, database } = config;
 if (process.env.NODE_ENV === 'production') {
@@ -18,8 +18,8 @@ if (process.env.NODE_ENV === 'production') {
 const app = express();
 app.use(express.json());
 
-logs(app);
-routes(app);
+log(app);
+route(app);
 
 app.listen(server.port, server.host, () => console.log(`Server has started on ${server.host}:${server.port}`));
 
